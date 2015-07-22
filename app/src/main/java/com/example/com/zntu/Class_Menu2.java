bbbcffec660caa8afc8c29391e97d3fcc8914f37
1injener <injenerzntu@gmail.com>
2015-07-22 22:44:44 +0400

    очередная проба с меню.

--- app/src/main/java/com/example/com/zntu/Class_Menu2.java
+++ app/src/main/java/com/example/com/zntu/Class_Menu2.java
@@ -2,34 +2,44 @@ package com.example.com.zntu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
  * Created by 1injener on 16.07.15.
  */
 public class Class_Menu2 extends Activity {
-
+    ListView listView = (ListView)findViewById(R.id.listViewDovidka);
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_dzin2);
 
 
-        // Получаем экземпляр элемента Spinner
-        final Spinner spinner_lay = (Spinner)findViewById(R.id.DovidnikSp);
+// определяем массив типа String
+        final String[] catnames = getResources().getStringArray(R.array.TelephoneList);
+// используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,	android.R.layout.simple_list_item_1, catnames);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                Toast.makeText(getApplicationContext(), ((TextView)itemClicked).getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
-// Настраиваем адаптер
-        ArrayAdapter<?> adapter =
-                ArrayAdapter.createFromResource(this, R.array.TelephoneList, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-// Вызываем адаптер
-        spinner_lay.setAdapter(adapter);
 
 
     }
+
 
 
 
