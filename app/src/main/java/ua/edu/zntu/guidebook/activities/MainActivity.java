3fce212237138a2af56264b0395a07f97f94826e
DenisBogatirov <denis.bogatirov@gmail.com>
2015-11-26 23:19:04 +0200

    layout resource to static final variable Added realisation of NavigationDrawer Added Toolbar menu

--- app/src/main/java/ua/edu/zntu/guidebook/activities/MainActivity.java
+++ app/src/main/java/ua/edu/zntu/guidebook/activities/MainActivity.java
@@ -1,29 +1,52 @@
 package ua.edu.zntu.guidebook.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import ua.edu.zntu.guidebook.R;
 
 public class MainActivity extends AppCompatActivity{
 
+
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         
         initToolbar();
+        initNavigationView();
 
     }
+
+
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
 
+        toolbar.inflateMenu(R.menu.menu_toolbar);
     }
 
+    private void initNavigationView() {
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+
+    }
 }
