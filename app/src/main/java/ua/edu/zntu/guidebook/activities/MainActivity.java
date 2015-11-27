203dfcb7e80d32e828ae408c545fb752db2c2843
DenisBogatirov <denis.bogatirov@gmail.com>
2015-11-27 22:39:25 +0200

    Added Fragment Support Established onNavigationItemSelectedListener

--- app/src/main/java/ua/edu/zntu/guidebook/activities/MainActivity.java
+++ app/src/main/java/ua/edu/zntu/guidebook/activities/MainActivity.java
@@ -1,29 +1,50 @@
 package ua.edu.zntu.guidebook.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import ua.edu.zntu.guidebook.R;
+import ua.edu.zntu.guidebook.fragments.TimetableFragment;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private TimetableFragment timetableFragment;
+
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
+
+
+
+        manager = getSupportFragmentManager();
+
+        timetableFragment = new TimetableFragment();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, timetableFragment).commit();
+        }
         
         initToolbar();
         initNavigationView();
     }
+
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -40,6 +61,43 @@ public class MainActivity extends AppCompatActivity{
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation);
 
+        navigationView.getMenu().getItem(0).setChecked(true);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        transaction = manager.beginTransaction();
+
+        switch (id){
+
+            case R.id.nav_timetable:
+
+                transaction.add(R.id.container, timetableFragment);
+
+                break;
+
+            case R.id.nav_find:
+
+                break;
+
+            case R.id.nav_news:
+
+                break;
+
+            case R.id.nav_directory:
+
+                break;
+        }
+
+        transaction.commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
