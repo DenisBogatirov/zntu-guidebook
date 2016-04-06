c6dbb0992cad6da234ff24abce2d30f532adbab5
Vitaliy <vitaliy.kononenko@freshcode.org>
2016-04-06 15:26:17 +0300

    added files for news CardView

---
+++ app/src/main/java/ua/edu/zntu/guidebook/adapters/NewsListAdapter.java
@@ -0,0 +1,6 @@
+package ua.edu.zntu.guidebook.adapters;
+
+/**
+ * Created by denis on 4/6/16.
+ */
+public class NewsListAdapter {}
---
+++ app/src/main/java/ua/edu/zntu/guidebook/dto/NewsDTO.java
@@ -0,0 +1,6 @@
+package ua.edu.zntu.guidebook.dto;
+
+/**
+ * Created by denis on 4/6/16.
+ */
+public class NewsDTO {}
