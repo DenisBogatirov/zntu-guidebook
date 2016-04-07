8eba30cbc943176a34ba4adf73f70615c53832db
Vitaliy <vitaliy.kononenko@freshcode.org>
2016-04-07 18:21:05 +0300

    Tmp file

---
+++ app/src/main/java/ua/edu/zntu/guidebook/dto/TodosDTO.java
@@ -0,0 +1,115 @@
+package ua.edu.zntu.guidebook.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo")
+public class TodosDTO {
+
+    private String type;
+    private String userId;
+    private String id;
+    private String title;
+    private String completed;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     *
+     * @return
+     * The type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     *
+     * @param type
+     * The type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     *
+     * @return
+     * The userId
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     *
+     * @param userId
+     * The userId
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     *
+     * @return
+     * The id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     * The id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     * The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @param title
+     * The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @return
+     * The completed
+     */
+    public String getCompleted() {
+        return completed;
+    }
+
+    /**
+     *
+     * @param completed
+     * The completed
+     */
+    public void setCompleted(String completed) {
+        this.completed = completed;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
