efe4383d35c69e6dce7911dd9cac9e9a5a89dfeb
Vitaliy <vitaliy.kononenko@freshcode.org>
2016-04-15 18:30:52 +0300

    Changed section_info fragment

---
+++ app/src/main/java/ua/edu/zntu/guidebook/adapters/SectionInfoAdapter.java
@@ -0,0 +1,6 @@
+package ua.edu.zntu.guidebook.adapters;
+
+/**
+ * Created by denis on 4/15/16.
+ */
+public class SectionInfoAdapter {}
---
+++ app/src/main/java/ua/edu/zntu/guidebook/pojo/Deanery.java
@@ -0,0 +1,6 @@
+package ua.edu.zntu.guidebook.pojo;
+
+/**
+ * Created by denis on 4/15/16.
+ */
+public class Deanery {}
