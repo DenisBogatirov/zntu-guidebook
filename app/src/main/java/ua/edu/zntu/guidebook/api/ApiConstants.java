69ee2636dc005c1569d4d018ee1256cdebede932
Vitaliy <vitaliy.kononenko@freshcode.org>
2016-04-18 14:44:49 +0300

    Changed BASE_URL

--- app/src/main/java/ua/edu/zntu/guidebook/api/ApiConstants.java
+++ app/src/main/java/ua/edu/zntu/guidebook/api/ApiConstants.java
@@ -1,6 +1,6 @@
 package ua.edu.zntu.guidebook.api;
 
 public class ApiConstants {
-    public static final String BASE_URL = "http://192.168.1.238/";
+    public static final String BASE_URL = "http://denisbogatirov.ho.ua/";
     public static final String GET_NEWS_URL = "/index.php?option=com_mobilenews";
 }
